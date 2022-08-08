83b8e81095e3fe28c18ead17e56b8728a7ad7015
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-08 10:26:04 +0530

    AddRole()

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,21 @@
 package com;
 
+import java.util.Scanner;
+
+import org.hibernate.Transaction;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import com.bean.RoleBean;
 
 /**
  * Hello world!
  *
  */
+
+	
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
@@ -20,6 +28,17 @@ public class App {
 
 		// use to execute query
 		Session session = factory.openSession();
-
+		
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter role name");
+		String roleName = scr.next();
+		
+		 Transaction tx = session.beginTransaction();
+		 
+		RoleBean roleBean = new RoleBean();
+		roleBean.setRoleName(roleName);
+		session.save(roleBean);
+		tx.commit();
+		session.close();
 	}
 }
