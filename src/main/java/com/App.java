adc8346bc14552d2a8542c44523a72e4f41d9505
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-08 10:42:55 +0530

    getRoleById()

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -19,63 +19,81 @@ import com.bean.RoleBean;
  *
  */
 
-	
 public class App {
-	
+
 	Configuration cfg = null;
 	SessionFactory factory = null;
-	
+
 	public App() {
-		
+
 		// use to create session factory from give configuration
 		cfg = new Configuration().configure("hibernate-config.xml");
-		
+
 		// use to create session
 		factory = cfg.buildSessionFactory();
 	}
-	
+
 	void addRole() {
-		
+
 		// use to execute query
 		Session session = factory.openSession();
-		
+
 		Scanner scr = new Scanner(System.in);
 		System.out.println("Enter role name");
 		String roleName = scr.next();
-		
+
 		RoleBean roleBean = new RoleBean();
 		roleBean.setRoleName(roleName);
-		
+
 		Transaction tx = session.beginTransaction();
-		
-		
+
 		try {
 			session.save(roleBean);
 			tx.commit();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			tx.rollback();// undo
 		}
 		session.close();
 	}
-	
+
 	void getAllRole() {
 		Session session = factory.openSession();
 		Query query = session.createQuery("from RoleBean");
-		
+
 		List<RoleBean> listRole = query.getResultList();
-		
+
 		System.out.println("RoleId \t RoleName");
 		for (RoleBean roleBean : listRole) {
-			System.out.println(roleBean.getRoleId()+"\t "+roleBean.getRoleName());
+			System.out.println(roleBean.getRoleId() + "\t " + roleBean.getRoleName());
 		}
 		session.close();
 	}
-	
+
+	void getRoleById() {
+
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter role Id");
+		int roleId = scr.nextInt();
+		
+		Session session = factory.openSession();
+				
+		RoleBean roleBean = session.get(RoleBean.class, roleId);
+		
+		if (roleBean == null) {
+			System.out.println("Invalid id please try again!!!");
+		} else {
+			System.out.println("===Role Detail=====");
+			System.out.println(roleBean.getRoleId());
+			System.out.println(roleBean.getRoleName());
+		}
+		session.close();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
+
 		int choice = 0;
 		App app = new App();
 
@@ -85,7 +103,7 @@ public class App {
 
 			Scanner scr = new Scanner(System.in);
 
-				choice = scr.nextInt();
+			choice = scr.nextInt();
 
 			switch (choice) {
 			case 0:
@@ -94,10 +112,14 @@ public class App {
 				app.addRole();
 				break;
 			case 2:
-				app.getAllRole();;
+				app.getAllRole();
 				break;
+			case 3:
+				app.getRoleById();
+				break;
+				
 			}// switch
 		} // while
-	
+
 	}
 }
