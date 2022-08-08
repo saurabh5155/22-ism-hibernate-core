17c6be1ba0b7a392bc1f0cffd61e310b43dfa128
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-08 10:31:25 +0530

    addRole() With switch case

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -17,15 +17,21 @@ import com.bean.RoleBean;
 
 	
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+	
+	Configuration cfg = null;
+	SessionFactory factory = null;
+	
+	public App() {
+		
 		// use to create session factory from give configuration
-		Configuration cfg = new Configuration().configure("hibernate-config.xml");
-
+		cfg = new Configuration().configure("hibernate-config.xml");
+		
 		// use to create session
-		SessionFactory factory = cfg.buildSessionFactory();
-
+		factory = cfg.buildSessionFactory();
+	}
+	
+	void addRole() {
+		
 		// use to execute query
 		Session session = factory.openSession();
 		
@@ -33,12 +39,46 @@ public class App {
 		System.out.println("Enter role name");
 		String roleName = scr.next();
 		
-		 Transaction tx = session.beginTransaction();
-		 
 		RoleBean roleBean = new RoleBean();
 		roleBean.setRoleName(roleName);
-		session.save(roleBean);
-		tx.commit();
+		
+		Transaction tx = session.beginTransaction();
+		
+		
+		try {
+			session.save(roleBean);
+			tx.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();// undo
+		}
 		session.close();
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		int choice = 0;
+		App app = new App();
+
+		while (true) {
+			System.out.println("\n0 for exit\n1 for add\n2 for list all roles\n3 for view\n4 for delete role");
+			System.out.println("Enter choice....");
+
+			Scanner scr = new Scanner(System.in);
+
+				choice = scr.nextInt();
+
+			switch (choice) {
+			case 0:
+				System.exit(0);
+			case 1:
+				app.addRole();
+				break;
+			}// switch
+		} // while
+	
 	}
 }
